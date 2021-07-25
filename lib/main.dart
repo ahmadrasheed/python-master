16e52f34f6c4cf0e2d3dfe397163b49754315266
ahmadrasheed <ahmad.rasheed1@yahoo.com>
2021-07-25 12:49:47 +0300

    after adding buttom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:python/screens/LandingScreen.dart';
-import 'package:get/get.dart';
-
-import 'package:python/screens/PinterestGrid.dart';
-import 'package:python/screens/quiz/quiz_screen.dart';
-import 'package:python/screens/welcome/welcome_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,75 +8,135 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Bottom Nav Bar V2',
       theme: ThemeData(
-
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      //home: PinterestGrid(),
-      getPages: [
-        GetPage(name: '/', page: () => PinterestGrid()),
-        GetPage(
-            name: '/quiz', page: () => QuizScreen()) // Dynamic route
-      ],//PinterestGrid()
-
+      home: BottomNavBarV2(),
     );
-
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class BottomNavBarV2 extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _BottomNavBarV2State createState() => _BottomNavBarV2State();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BottomNavBarV2State extends State<BottomNavBarV2> {
+  int currentIndex = 0;
 
-  void _incrementCounter() {
+  setBottomBarIndex(index) {
     setState(() {
-     // _counter++;
-      Get.toNamed('/quiz');
+      currentIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      /*appBar: AppBar(
-
-        title: Text(widget.title),
-      ),*/
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.white.withAlpha(55),
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Container(
+              width: size.width,
+              height: 80,
+              child: Stack(
+                overflow: Overflow.visible,
+                children: [
+                  CustomPaint(
+                    size: Size(size.width, 80),
+                    painter: BNBCustomPainter(),
+                  ),
+                  Center(
+                    heightFactor: 0.6,
+                    child: FloatingActionButton(backgroundColor: Colors.orange, child: Icon(Icons.shopping_basket), elevation: 0.1, onPressed: () {}),
+                  ),
+                  Container(
+                    width: size.width,
+                    height: 80,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.home,
+                            color: currentIndex == 0 ? Colors.orange : Colors.grey.shade400,
+                          ),
+                          onPressed: () {
+                            setBottomBarIndex(0);
+                          },
+                          splashColor: Colors.white,
+                        ),
+                        IconButton(
+                            icon: Icon(
+                              Icons.restaurant_menu,
+                              color: currentIndex == 1 ? Colors.orange : Colors.grey.shade400,
+                            ),
+                            onPressed: () {
+                              setBottomBarIndex(1);
+                            }),
+                        Container(
+                          width: size.width * 0.20,
+                        ),
+                        IconButton(
+                            icon: Icon(
+                              Icons.bookmark,
+                              color: currentIndex == 2 ? Colors.orange : Colors.grey.shade400,
+                            ),
+                            onPressed: () {
+                              setBottomBarIndex(2);
+                            }),
+                        IconButton(
+                            icon: Icon(
+                              Icons.notifications,
+                              color: currentIndex == 3 ? Colors.orange : Colors.grey.shade400,
+                            ),
+                            onPressed: () {
+                              setBottomBarIndex(3);
+                            }),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class BNBCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+
+    Path path = Path();
+    path.moveTo(0, 20); // Start
+    path.quadraticBezierTo(size.width * 0.20, 0, size.width * 0.35, 0);
+    path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 20);
+    path.arcToPoint(Offset(size.width * 0.60, 20), radius: Radius.circular(20.0), clockwise: false);
+    path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
+    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 20);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 20);
+    canvas.drawShadow(path, Colors.black, 5, true);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
